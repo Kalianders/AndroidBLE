b38bb6ad920e530928e025340de740f1b104a7a3
Joonas Isometsä <joonas.isometsa@aalto.fi>
2016-07-05 16:04:50 +0300

    korjailua

---
+++ Ownapps/gatt_db.h
@@ -0,0 +1,12 @@
+#ifndef BG_GATTDB_H
+#define BG_GATTDB_H
+
+#include "ubt/bg_gattdb_def.h"
+
+extern const struct bg_gattdb_def bg_gattdb_data;
+
+#define xgatt_lloss                            15
+#define xgatt_alert                            18
+#define xgatt_txpower                          21
+
+#endif
