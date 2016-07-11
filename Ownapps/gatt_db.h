cd2a0524b14181e9edbbe46a206947b40ce49038
Joonas Isometsä <joonas.isometsa@aalto.fi>
2016-07-12 01:39:44 +0300

    Laaja Bgsrcipt päivitys

--- Ownapps/gatt_db.h
+++ Ownapps/gatt_db.h
@@ -9,5 +9,7 @@ extern const struct bg_gattdb_def bg_gattdb_data;
 #define xgatt_alert                            18
 #define xgatt_txpower                          21
 #define xgatt_adc_voltage                      24
+#define control_byte                           29
+#define status_vector                          32
 
 #endif
---
+++ adc_test_bgm111_devkit/gatt_db.h
@@ -0,0 +1,10 @@
+#ifndef BG_GATTDB_H
+#define BG_GATTDB_H
+
+#include "ubt/bg_gattdb_def.h"
+
+extern const struct bg_gattdb_def bg_gattdb_data;
+
+#define xgatt_adc_voltage                      15
+
+#endif
