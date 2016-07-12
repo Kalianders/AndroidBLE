50657a199f75a721f15cd1c89c2c4f9860d46077
joonabs <joonas.isometsa@aalto.fi>
2016-07-12 21:14:17 +0300

    päivitystä

--- Ownapps/gatt_db.h
+++ Ownapps/gatt_db.h
@@ -5,11 +5,10 @@
 
 extern const struct bg_gattdb_def bg_gattdb_data;
 
-#define xgatt_lloss                            15
-#define xgatt_alert                            18
-#define xgatt_txpower                          21
-#define xgatt_adc_voltage                      24
-#define control_byte                           29
-#define status_vector                          32
+#define xgatt_alert                            15
+#define xgatt_minvalue                         18
+#define xgatt_maxvalue                         20
+#define xgatt_gapsize                          22
+#define xgatt_distance_onoff                   24
 
 #endif
